cf350eaa74c835ff46875ba40ad9afb54367652a
Danial <danialzafarfrpf@gmail.com>
2022-09-30 22:55:53 +0500

    make it better

---
+++ server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import cors from "cors";
+import getPagesAvgPrice from "./lib/puppeteer/getPagesAvgPrice.js";
+
+const app = express();
+
+// Heroku Port
+const PORT = process.env.PORT || 3000;
+
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET"],
+    })
+);
+
+app.get("/", (_req, res) => {
+    res.send("server is running");
+});
+
+app.get("/:maxPages/:search", async (req, res) => {
+    const { search, maxPages } = req.params;
+    try {
+        // Check wether the max page number is valid
+        if (!parseInt(maxPages) || parseInt(maxPages) < 0) {
+            res.status(300).json({
+                message: "Max page should be a positive number",
+            });
+            return;
+        }
+
+        // Calculate average price
+        const averagePrice = await getPagesAvgPrice(search, parseInt(maxPages));
+        res.json({ message: "Yay", averagePrice });
+        return;
+    } catch (e) {
+        console.log(e);
+        res.json({
+            message: "Either no product for search or server error",
+            error: e.message,
+        });
+        return;
+    }
+});
+
+app.get("*", (_req, res) => {
+    res.status(404).json({ message: "Not available" });
+});
+
+app.listen(PORT);
